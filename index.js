1f7c9cc281ba6368481c457348c0bd1949c73df5
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-06-22 07:51:17 +0200

    update(): move remote resolver into resolvers folder

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var DependencyResolver = require('./lib/dependency-resolver');
-var RemoteResolver = require('./lib/remote-resolver');
+var RemoteResolver = require('./lib/resolvers/remote-resolver');
 
 var dependencies = new DependencyResolver();
 var remote = new RemoteResolver();
