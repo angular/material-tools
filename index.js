43a1e1b28bf6f673efcc1d4b209ef62044e4ed59
Paul Gschwendtner <paulgschwendtner@gmail.com>
2016-06-22 08:07:58 +0200

    chore(): use typescript for toolings

--- index.js
+++
@@ -1,7 +0,0 @@
-var DependencyResolver = require('./lib/dependency-resolver');
-var RemoteResolver = require('./lib/resolvers/remote-resolver');
-
-var dependencies = new DependencyResolver();
-var remote = new RemoteResolver();
-
-console.log(dependencies.resolve());
