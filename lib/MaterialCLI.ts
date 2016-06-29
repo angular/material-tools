c4cccb9b1e650be5e6c935feab85984cb2c4821f
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2016-06-29 15:15:45 +0200

    feat(material-cli): add the CLI entry point (#35)
    * feat(material-cli): add the CLI entry point
    
    Adds the entry point for the CLI.
    
    * chore(material-tools): add typings and `bin` to the package.json
    
    * chore(material-tools): add shebang and rename the bin file
    
    * chore(material-cli): comment out the shebang since tslint complains

---
+++ lib/MaterialCLI.ts
@@ -0,0 +1,29 @@
+// #! /usr/bin/env node
+
+import {MaterialTools} from './MaterialTools';
+
+let options = require('commander');
+
+options
+  .option('-v, --version [version]', 'Angular Material version.')
+  .option('-m, --modules <list>', 'Comma-separated list of modules to be included in the build.', list => list.split(','))
+  .option('--cache [directory]', 'Directory to be used as a cache for downloaded versions.')
+  .option('--main-filename [name]', 'File to be used to figure out the dependencies between modules.')
+  .parse(process.argv);
+
+// `commander` has a `version` method on it's prototype. This means that if no `version`
+// is specified, it would get looked up the prototype chain. In this case we define
+// an own, undefined `version` property so the MaterialTools can properly fall back
+// to the default.
+if (!options.hasOwnProperty('version')) {
+  Object.defineProperty(options, 'version', {
+    writable: true
+  });
+}
+
+const tools = new MaterialTools(options);
+
+tools
+  .getFiles()
+  .then(result => console.log(result))
+  .catch(error => console.error(error));
