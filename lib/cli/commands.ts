30d6fdf62cca09e038c6ac87391ef490fdc55ba9
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2016-07-13 20:30:54 +0200

    feat(material-cli): allow for multiple themes to be passed in the CLI (#126)
    * feat(material-cli): allow for multiple themes to be passed in the CLI
    
    Adds the ability for users to pass multiple themes in the CLI by specifying them in the format of `--primary <primary palette for theme 1> <primary palette for theme N> --accent <accent palette for theme 1> <accent palette for theme N>`.
    
    Fixes #119.
    
    * fix(material-cli): add comments
    
    * fix(material-cli): typo

--- lib/cli/commands.ts
+++ lib/cli/commands.ts
@@ -20,7 +20,7 @@ export function registerCommands(yargs: any): any {
           alias: key.split('-').shift(),
           describe: PALETTE_OPTIONS[key],
           group: THEMING_GROUP,
-          type: 'string',
+          type: 'array',
           requiresArg: true
         });
       });
@@ -31,18 +31,54 @@ export function registerCommands(yargs: any): any {
       });
 
       return markAsStrict(command);
-    }, args => {
-      // Turn the flat theme arguments into a theme object.
-      args.theme = {
-        dark: !!args.dark
-      };
-
-      ['primaryPalette', 'accentPalette', 'warnPalette', 'backgroundPalette'].forEach(name => {
-        if (typeof args[name] !== 'undefined') {
-          args.theme[name] = args[name];
-        }
-      });
-    });
+    }, processThemeArgs);
 
   return yargs;
 }
+
+/** Turns the specified theme options into an array, expected by the theming builder. */
+function processThemeArgs(args) {
+  const availablePalettes = ['primaryPalette', 'accentPalette', 'warnPalette', 'backgroundPalette'];
+  let themes = [];
+
+  // Filter out the theme-specific keys from the rest.
+  let definedPalettes = Object.keys(args)
+    .filter(key => availablePalettes.indexOf(key) !== -1);
+
+  // Find the palette with the most colors to be used as a reference.
+  let largestPalette = definedPalettes
+    .sort((key, prevKey) => args[prevKey].length - args[key].length)[0];
+
+  if (largestPalette) {
+    /*
+      Goes through the colors and creates new themes by matching them to their index.
+      E.g. { primaryPalette: ['red', 'green'], accentPalette: ['blue', 'yellow'], turns into
+      [
+        { primaryPalette: 'red', accentPalette: 'blue' },
+        { primaryPalette: green, accentPalette: 'yellow' }
+      ]
+     */
+    for (let i = 0; i < args[largestPalette].length; i++) {
+      let newTheme = {
+        dark: !!args.dark
+      };
+
+      definedPalettes.forEach(palette => {
+        let value = args[palette][i];
+
+        if (value) {
+          newTheme[palette] = value;
+        }
+      });
+
+      themes.push(newTheme);
+    }
+  } else {
+    // Add a default theme, that will use the defaults, if no palettes were specified.
+    themes.push({
+      dark: !!args.dark
+    });
+  }
+
+  args.theme = themes;
+};
