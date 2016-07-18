8fe18f11d0594f394846470ad14a716fbf6c4480
Paul Gschwendtner <paulgschwendtner@me.com>
2016-07-18 21:53:01 +0200

    test(): enhance amount of tests (#9)
    * test(): enhance amount of tests
    
    * Add more tests regarding the VirtualContext / SandboxRequire
    * Fix unchecked lintings for some spec files.
    
    * Move path to the top

--- tests/VirtualContext.spec.ts
+++ tests/VirtualContext.spec.ts
@@ -1,5 +1,6 @@
-import {VirtualContext} from '../lib/virtual_context/VirtualContext';
 import * as path from 'path';
+import {VirtualContext} from '../lib/virtual_context/VirtualContext';
+import {BrowserWindow} from '../lib/virtual_context/MockBrowser';
 
 describe('virtual context', () => {
 
@@ -29,25 +30,36 @@ describe('virtual context', () => {
     expect(exports.require).toBeTruthy();
   });
 
+  it('should add browser mocks to the globals', () => {
+    let fakeBrowser = new BrowserWindow();
+
+    let results = Object.keys(fakeBrowser).filter(property => {
+      return !this.virtualContext.globals.hasOwnProperty(property);
+    });
+
+    expect(results.length).toBe(0);
+  });
+
   describe('with block scope scripts', () => {
 
     it('should run properly when strict mode is manually enabled', () => {
-      let filePath = path.normalize(__dirname + '/fixtures/virtual-context/fake-virtual-context-strict-mode.js');
+      let filePath = __dirname + '/fixtures/virtual-context/fake-virtual-context-strict-mode.js';
 
       let numbers = this.virtualContext.run(filePath, {
         strictMode: true
       });
 
-      // The strict mode file will export an array of 50 numbers by using the block scope (strict mode)
+      // The strict mode file will export an array of 50 numbers by using the block scope
+      // (strict mode)
       expect(numbers.length).toBe(50);
     });
 
 
     it('should throw an error when strict mode is disabled', () => {
-      let filePath = path.normalize(__dirname + '/fixtures/virtual-context/fake-virtual-context-strict-mode.js');
+      let filePath = __dirname + '/fixtures/virtual-context/fake-virtual-context-strict-mode.js';
 
-      // The virtual context will throw an error, because the file uses block-scoped declarations outside of
-      // strict mode.
+      // The virtual context will throw an error, because the file uses block-scoped declarations
+      // outside of strict mode.
       expect(() => {
         this.virtualContext.run(filePath);
       }).toThrow();
